cd40f639251f0c27b88e5d9409d392f19a0ddf3f
Jason Park <jjpark987@gmail.com>
2023-07-28 08:21:51 -0700

    commit tree cd5ee9e963ffedbdf16470541e3cfd24555448e1

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,30 @@
 require_relative './stack'
 
-# your code here
+class MyQueue
+    def initialize
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end
+
+    def add value
+        @s1.push value
+    end
+
+    def remove 
+        transfer
+        @s2.pop
+    end
+
+    def peek 
+        transfer
+        @s2.peek
+    end
+
+    private
+
+    def transfer
+        until @s1.empty?
+            @s2.push @s1.pop
+        end
+    end
+end
